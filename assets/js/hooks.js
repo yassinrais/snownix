e42aa2c5b23ae14798e59826c36a2248161312be
Yassine Rais <yassmailrais@gmail.com>
2022-03-15 10:16:39 +0100

    Posts Scrolling on update

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -15,6 +15,11 @@ const Hooks = {
                 window.location = parser.href;
             });
         }
+    },
+    ScrollOnUpdate: {
+        updated() {
+            this.el.scrollIntoView();
+        }
     }
 }
 
